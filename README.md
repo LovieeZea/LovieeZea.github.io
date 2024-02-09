3a21872857ebb6420d77dcedcfbd7a2af6ba18b1
bebekosizea <98525253+LovieeZea@users.noreply.github.com>
2024-02-09 18:44:23 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# impressingCrush
-link mobile.js instead of script.js in index.html to work in mobile.
-
-Thanks and Happy Coding.
+# Forgive-Me
--- script.js
+++ script.js
@@ -1,63 +1,21 @@
-"use strict";
+let currentPage = 1;
 
-const titleElement = document.querySelector(".title");
-const buttonsContainer = document.querySelector(".buttons");
-const yesButton = document.querySelector(".btn--yes");
-const noButton = document.querySelector(".btn--no");
-const catImg = document.querySelector(".cat-img");
-
-const MAX_IMAGES = 5;
-
-let play = true;
-let noCount = 0;
-
-yesButton.addEventListener("click", handleYesClick);
-
-noButton.addEventListener("click", function () {
-  if (play) {
-    noCount++;
-    const imageIndex = Math.min(noCount, MAX_IMAGES);
-    changeImage(imageIndex);
-    resizeYesButton();
-    updateNoButtonText();
-    if (noCount === MAX_IMAGES) {
-      play = false;
-    }
+function toggleClass(e, toggleClassName) {
+  if (e.className.includes(toggleClassName)) {
+    e.className = e.className.replace(" " + toggleClassName, "");
+  } else {
+    e.className += " " + toggleClassName;
   }
-});
-
-function handleYesClick() {
-  titleElement.innerHTML = "Iloveyou babiii, if you've reach here po tell me if done and ill send u link hehe";
-  buttonsContainer.classList.add("hidden");
-  changeImage("yes");
 }
 
-function resizeYesButton() {
-  const computedStyle = window.getComputedStyle(yesButton);
-  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
-  const newFontSize = fontSize * 1.6;
-
-  yesButton.style.fontSize = `${newFontSize}px`;
-}
-
-function generateMessage(noCount) {
-  const messages = [
-    "No",
-    "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
-  ];
-
-  const messageIndex = Math.min(noCount, messages.length - 1);
-  return messages[messageIndex];
-}
-
-function changeImage(image) {
-  catImg.src = `img/cat-${image}.jpg`;
-}
-
-function updateNoButtonText() {
-  noButton.innerHTML = generateMessage(noCount);
+function movePage(e, page) {
+  if (page == currentPage) {
+    currentPage += 2;
+    toggleClass(e, "left-side");
+    toggleClass(e.nextElementSibling, "left-side");
+  } else if ((page = currentPage - 1)) {
+    currentPage -= 2;
+    toggleClass(e, "left-side");
+    toggleClass(e.previousElementSibling, "left-side");
+  }
 }
